3be0be39c3c493c076d734d80305340b6d397c04
Kai Yamada <gpioblink@aol.com>
2020-02-28 05:09:04 +0900

    add stripe service

---
+++ pkg/payments/infrastructure/payments/stripe.go
@@ -0,0 +1,52 @@
+package payments
+
+import (
+	"fmt"
+	"github.com/gpioblink/go-stripe-book-seller/pkg/common/price"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/checkout/session"
+)
+
+type ConfirmedPayment struct {
+	orderID string
+	Price   price.Price
+}
+
+type StripeService struct {
+	// TODO: product情報を取得して決済画面で商品の詳細を表示できるようにする
+}
+
+func NewStripeService(apiKey string) (StripeService, error) {
+	stripe.Key = apiKey
+	return StripeService{}, nil
+}
+
+func (s StripeService) InitPaymentProvider(orderID string, price price.Price) error {
+	//TODO: need editing
+
+	params := &stripe.CheckoutSessionParams{
+		PaymentMethodTypes: stripe.StringSlice([]string{
+			"card",
+		}),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				Name:        stripe.String(fmt.Sprintf("Item %s", orderID)),
+				Description: stripe.String("Sample product"),
+				Amount:      stripe.Int64(int64(price.Cents())),
+				Currency:    stripe.String(price.Currency()),
+				Quantity:    stripe.Int64(1),
+			},
+		},
+		SuccessURL: stripe.String("https://example.com/success?session_id={CHECKOUT_SESSION_ID}"),
+		CancelURL:  stripe.String("https://example.com/cancel"),
+	}
+	params.SetIdempotencyKey(orderID)
+	params.AddMetadata("order_id", orderID)
+
+	_, err := session.New(params)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
