d74f5efef28b1bdea6f08410156aa33df50755aa
Kai Yamada <gpioblink@aol.com>
2020-02-28 17:21:43 +0900

    fix value

--- pkg/printer/application/printer.go
+++ pkg/printer/application/printer.go
@@ -26,15 +26,15 @@ func (p PrinterService) PrintReceipt(orderID string, name string, price int,
 		return err
 	}
 
-	err = p.printService.PrintTitle(fmt.Sprintf("購入番号: %s", orderID))
+	err = p.printService.PrintLine(fmt.Sprintf("購入番号: %s", orderID))
 	if err != nil {
 		return err
 	}
-	err = p.printService.PrintTitle(fmt.Sprintf("商品名: %s", name))
+	err = p.printService.PrintLine(fmt.Sprintf("商品名: %s", name))
 	if err != nil {
 		return err
 	}
-	err = p.printService.PrintTitle(fmt.Sprintf("金額: %d円", price))
+	err = p.printService.PrintLine(fmt.Sprintf("金額: %d円", price))
 	if err != nil {
 		return err
 	}
